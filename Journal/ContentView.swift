73887e599732a115a5e5056aff2b6dc525af52ed
Lachlan Campbell <lachlan@hackclub.com>
2020-03-07 18:08:33 -0500

    Begin app

--- Journal/ContentView.swift
+++ Journal/ContentView.swift
@@ -7,11 +7,13 @@
 //
 
 import SwiftUI
+import UIKit
+import PencilKit
 
 private let dateFormatter: DateFormatter = {
     let dateFormatter = DateFormatter()
     dateFormatter.dateStyle = .medium
-    dateFormatter.timeStyle = .medium
+    dateFormatter.timeStyle = .none
     return dateFormatter
 }()
 
@@ -21,7 +23,7 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             MasterView(dates: $dates)
-                .navigationBarTitle(Text("Master"))
+                .navigationBarTitle(Text("Journal"))
                 .navigationBarItems(
                     leading: EditButton(),
                     trailing: Button(
@@ -29,7 +31,7 @@ struct ContentView: View {
                             withAnimation { self.dates.insert(Date(), at: 0) }
                         }
                     ) {
-                        Image(systemName: "plus")
+                        Image(systemName: "square.and.pencil")
                     }
                 )
             DetailView()
@@ -57,15 +59,34 @@ struct MasterView: View {
 
 struct DetailView: View {
     var selectedDate: Date?
-
+    @State var color = UIColor.black
+    @State var clear = false
+    
     var body: some View {
-        Group {
-            if selectedDate != nil {
-                Text("\(selectedDate!, formatter: dateFormatter)")
-            } else {
-                Text("Detail view content goes here")
+        NavigationView {
+            VStack {
+                PKCanvas(color: $color, clear: $clear)
+                HStack {
+                    Button("Red") { self.color = UIColor.systemRed }.accentColor(.red)
+                    Button("Orange") { self.color = UIColor.systemOrange }.accentColor(.orange)
+                    Button("Green") { self.color = UIColor.systemGreen }.accentColor(.green)
+                    Button("Blue") { self.color = UIColor.systemBlue }.accentColor(.blue)
+                    Button("Black") { self.color = UIColor.black }.accentColor(.primary)
+                    Spacer()
+                    Button("Clear Canvas") { self.clear.toggle() }
+                }.padding()
             }
-        }.navigationBarTitle(Text("Detail"))
+            .navigationBarTitle(
+                selectedDate != nil
+                    ? Text("\(selectedDate!, formatter: dateFormatter)")
+                    : Text("No date selected")
+            ).navigationBarItems(trailing: HStack {
+                Button(action: {
+                }) {
+                    Image(systemName: "square.and.arrow.up")
+                }.padding(0)
+            })
+        }.navigationViewStyle(StackNavigationViewStyle())
     }
 }
 
@@ -73,5 +94,37 @@ struct DetailView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct PKCanvas: UIViewRepresentable {
+    class Coordinator: NSObject, PKCanvasViewDelegate {
+        var pkCanvas: PKCanvas
+
+        init(_ pkCanvas: PKCanvas) {
+            self.pkCanvas = pkCanvas
+        }
+    }
+
+    @Binding var color: UIColor
+    @Binding var clear: Bool
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    func makeUIView(context: Context) -> PKCanvasView {
+        let canvas = PKCanvasView()
+        canvas.tool = PKInkingTool(.pen, color: color, width: 10)
+
+        canvas.delegate = context.coordinator
+        return canvas
+    }
+
+    func updateUIView(_ canvasView: PKCanvasView, context: Context) {
+        if (clear != context.coordinator.pkCanvas.clear) {
+            canvasView.drawing = PKDrawing()
+        }
+        canvasView.tool = PKInkingTool(.pen, color: color, width: 10)
     }
 }
